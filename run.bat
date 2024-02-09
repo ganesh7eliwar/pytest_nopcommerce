a41d8ee2b760022616d33872aafd6e342c422e1f
ganesh7eliwar <ganesh7eliwar@gmail.com>
2024-02-09 16:35:41 +0530

    modified run.bat file

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-pytest -v --alluredir="allure-reports" --html=html_reports/run_bat.html --browser chrome -n=4 -p no:warings
+pytest -v --alluredir="allure-reports" --html=html_reports/run_bat.html --browser chrome -n=4 -p no:warnings
